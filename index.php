4b43823805664e9c8713757d90a9b48b919e6ce5
Akila Shashimantha <akilashashimantha84@gmail.com>
2024-11-08 20:27:30 +0530

    change

--- index.php
+++ index.php
@@ -17,16 +17,16 @@ include 'header.php';
 <div class=" container-fluid">
 <div class=" row">
 
-<div class="col-12 " style=" background-color: black;">
+<div class="col-12 p-0 my-0" style=" background-color: black;">
 
 <div class=" row ">
 
-<div class="col-lg-6 col-12" style="background-image: linear-gradient(to left top, #000000, #140d11, #1e161d, #251e2b, #28283b);">
+<div class="col-lg-6 col-12 my-0" style="background-image: linear-gradient(to left top, #000000, #140d11, #1e161d, #251e2b, #28283b);">
 
-<div class="col-12 d-flex justify-content-center waviy mt-3 my-5" >
+<div class="col-12 d-flex justify-content-center waviy mt-3 my-5  " >
 
-<span style="--i:1">C</span>
-   <span style="--i:2">R</span>
+<span  style="--i:1">C</span>
+   <span  style="--i:2">R</span>
    <span style="--i:3">E</span>
    <span style="--i:4">A</span>
    <span style="--i:5">T</span>
@@ -43,15 +43,15 @@ include 'header.php';
 <div class=" col-12 d-flex justify-content-center mt-5">
 <div class=" row">
 <div class=" col-6 fs-3 d-flex justify-content-center" style=" color: white;">Hi, I'm</div>
-<div class=" col-12 d-flex justify-content-center"><span style=" color:#F7E10A ;" class=" fs-1 mx-3">Akila Shashimantha</span></div>
+<div class=" col-12 d-flex justify-content-center skill"><span style=" color:#F7E10A ;" class=" fs-1 mx-3">Akila Shashimantha</span></div>
 
-<div class=" col-12 d-flex justify-content-center"><p class=" fs-3" style=" color: white;">Web App Developer</p></div>
+<div class=" col-12 d-flex justify-content-center my-0"><p class=" fs-3" style=" color: white;">Web App Developer</p></div>
 </div>
 </div>
 
 </div>
 
-<div class=" col-lg-6 col-12 d-flex justify-content-center p-2 mt-lg-0 mt-3" style=" background-image: linear-gradient(to right top, #000000, #140d11, #1e161d, #251e2b, #28283b);">
+<div class=" col-lg-6 col-12 d-flex justify-content-center p-2 mt-lg-0 " style=" background-image: linear-gradient(to right top, #000000, #140d11, #1e161d, #251e2b, #28283b);">
 
 <div class=" col-10 col-lg-7 circle-image" >
 <!-- My Image -->
@@ -82,7 +82,7 @@ include 'header.php';
 <div class="col-12 d-flex justify-content-center my-0 p-0">
     <h2 class=" my-0 mt-lg-3" style=" color: white; font-family:'Times New Roman', Times, serif;">About Me</h2>
 </div>
-<div class="col-12 p-0 m-0" style=" color: white;">
+<div class="col-12 p-2 m-0" style=" color: white;">
 
 <p class="p-0 m-0" style=" color: #B4B4C4;">Enthusiastic and focused Information Technology undergraduate at Horizon
 Campus, with a solid background in computer science and a keen interest in
@@ -130,16 +130,16 @@ challenges and is eager to bring creative solutions as part of a team.</p>
 <div class=" row d-flex justify-content-center services">
 <div class=" col-12 d-flex justify-content-center"><h2 style=" color: white;">My Services</h2></div>
 <!-- service card -->
-<div class="col-lg-4 col-6 d-flex justify-content-center my-5 p-2 service">
+<div class="col-lg-2 col-6 d-flex justify-content-center my-5 p-2 service">
 <div class=" row">
 
-<div class="col-12 d-flex justify-content-center" ><h3 style=" color: white;">Web Design</h3></div>
+<div class="col-12 d-flex justify-content-center skill" ><h3 style=" color: white;">Web Design</h3></div>
 
-<div class=" col-12 d-flex justify-content-center">
-<p class="d-flex justify-content-center" style=" color: #B4B4C4;">I'm currently design custom websites with HTMl,CSS,JavaScript,PHP and MySQl 
+<div class=" col-12 d-flex justify-content-center ">
+<p class="d-flex justify-content-center p-2" style=" color: #B4B4C4;">I'm currently design custom websites with HTMl,CSS,JavaScript,PHP and MySQl 
 and also make websites with WordPress Themes as your wish. I always try to do my level best to complete your wesite creatively.</p>
 </div>
-<div class=" col-12 mt-2 d-flex justify-content-center" style=" color: #B4B4C4;">
+<div class=" col-12 mt-2 d-flex justify-content-center " style=" color: #B4B4C4;">
 <p>Let me to create your wonderful website</p>
 </div>
 
